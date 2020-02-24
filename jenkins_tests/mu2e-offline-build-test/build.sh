c9d6b83c95db6af0c0c546d7281f00c4e0d564db
Ryunosuke O'Neil <ryunoneil@gmail.com>
2020-02-24 22:12:12 +0000

    build test job script

---
+++ jenkins_tests/mu2e-offline-build-test/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function do_setupstep() {
+    source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+    setup mu2e
+    setup codetools
+
+    # building prof or debug
+    ./buildopts --build=$BUILDTYPE
+    source setup.sh
+
+    return 0
+}
+
+function do_buildstep() {
+    scons --debug=time -k -j 16 2>&1 | tee "${WORKSPACE}/scons.log"
+    return ${PIPESTATUS[0]}
+}
+
+function do_runstep() {
+    mu2e -n 10 -c Validation/fcl/ceSimReco.fcl 2>&1 | tee "${WORKSPACE}/ceSimReco.log"
+    return ${PIPESTATUS[0]}
+}
+
+# dump the rev
+git show
+git rev-parse HEAD
+
+cd $WORKSPACE
+cd $REPO
+
+# dump the rev
+git show
+git rev-parse HEAD
+
+echo "["`date`"] setup"
+
+do_setupstep
+
+echo "["`date`"] ups"
+ups active
+
+echo "["`date`"] build"
+do_buildstep
+
+export SCONS_RC=$?
+echo "["`date`"] scons return code is $SCONS_RC"
+
+if [ $SCONS_RC -ne 0 ]; then
+  exit 1;
+fi
+
+do_runstep
+export CESIMRECO_RC=$?
+echo "["`date`"] ceSimReco return code is $CESIMRECO_RC"
