c0ca9d9d092d95611e511e2dd40010f1bb404a12
Ryunosuke O'Neil <roneil@fnal.gov>
2020-02-24 23:45:57 +0000

    clang tidy and formatting check scripts

---
+++ jenkins_tests/mu2e-offline-code-checks/formatting.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "[`date`] clang-format"
+
+PATCH_FILE="$WORKSPACE/clang-format-pr${PULL_REQUEST}-${COMMIT_SHA}.patch"
+
+clang-format -i $MODIFIED_PR_FILES
+git diff > $PATCH_FILE
+
+if [ -s "$PATCH_FILE" ]; then
+  PURL="${JOB_URL}/${BUILD_NUMBER}/artifact/clang-format-pr${PULL_REQUEST}-${COMMIT_SHA}.patch"
+
+  cat > $WORKSPACE/gh-report.md <<- EOM
+${COMMIT_SHA}
+mu2e/codechecks
+failure
+The code checks failed.
+${JOB_URL}/${BUILD_NUMBER}/console
+:-1: Code formatting checks have failed at ref ${COMMIT_SHA}.
+\`\`\`
+git diff --compact-summary
+$(git diff --compact-summary)
+\`\`\`
+
+\`clang-format\` made changes to files modified by this PR. Please run:
+\`\`\`
+clang-format -i ${MODIFIED_PR_FILES}
+git commit -am "Code formatting on ${COMMIT_SHA}"
+git push
+\`\`\`
+
+
+Alternatively, please review and \`git apply\` [this patch]($PURL) on your PR branch:
+\`\`\`
+curl $PURL | git apply -v --index
+git commit -am "Code formatting patch on ${COMMIT_SHA}"
+git push
+\`\`\`
+
+To avoid this in future please \`clang-format -i\` modified files before opening a PR.
+
+EOM
+    exit 1;
+fi
+
+exit 0;
