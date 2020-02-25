bcaf6e04cbf6dc1424dbb42e3376cdeba8e24638
Ryunosuke O'Neil <roneil@fnal.gov>
2020-02-25 00:14:38 +0000

    add iwyu and compdb generation to job.sh

--- jenkins_tests/mu2e-offline-code-checks/job.sh
+++ jenkins_tests/mu2e-offline-code-checks/job.sh
@@ -11,6 +11,9 @@ function do_setupstep() {
     return 0
 }
 
+function gen_compdb() {
+    python "$WORKSPACE/clangtools_utilities/gen_compdb.py"
+}
 
 echo "[$(date)] setup job environment"
 . setup.sh
@@ -28,9 +31,9 @@ git checkout ${COMMIT_SHA}
 echo "[$(date)] setups"
 do_setupstep
 
-MODIFIED_PR_FILES=`git diff --name-status master | grep "^M" | sed -e 's/^\w*\ *//' | awk '{$1=$1;print}'`
-echo "[$(date)] check formatting"
+export MODIFIED_PR_FILES=`git diff --name-status master | grep "^M" | sed -e 's/^\w*\ *//' | awk '{$1=$1;print}'`
 
+echo "[$(date)] check formatting"
 (
     source jenkins_tests/mu2e-offline-code-checks/formatting.sh
 )
@@ -40,6 +43,10 @@ if [ $? -ne 0 ]; then
 fi
 git reset --hard ${COMMIT_SHA}
 
+echo "[$(date)] setup compile_commands.json"
+gen_compdb
+
+echo "[$(date)] clang-tidy"
 (
     source jenkins_tests/mu2e-offline-code-checks/clangtidy.sh
 )
@@ -47,7 +54,15 @@ if [ $? -ne 0 ]; then
     cmsbot_report $WORKSPACE/gh-report.md
     exit 1;
 fi
+echo "[$(date)] include-what-you-use"
 
+(
+    source jenkins_tests/mu2e-offline-code-checks/iwyu.sh
+)
+if [ $? -ne 0 ]; then
+    cmsbot_report $WORKSPACE/gh-report.md
+    exit 0;
+fi
 
 cat > $WORKSPACE/gh-report.md <<- EOM
 ${COMMIT_SHA}
