07d066c46cf202e514682927a6998669bcb7f326
Ryunosuke O'Neil <roneil@fnal.gov>
2020-04-14 08:52:41 +0100

    Now we only run clang-tidy!

--- bin/github/jenkins_tests/mu2e-offline-code-checks/checks.sh
+++ bin/github/jenkins_tests/mu2e-offline-code-checks/checks.sh
@@ -14,14 +14,16 @@ CT_FILES="" # files to run in clang tidy
 for MOD_FILE in $MODIFIED_PR_FILES
 do
     if [[ "$MOD_FILE" == *.cc ]] || [[ "$MOD_FILE" == *.hh ]]; then
-        clang-format -i $MOD_FILE
-        echo "clang-format on $MOD_FILE"
+        # CLANG-FORMAT DISABLED!
+        #clang-format -i $MOD_FILE
+        #echo "clang-format on $MOD_FILE"
 
+        # we only wish to process .cc files
         if [[ "$MOD_FILE" == *.cc ]]; then
             CT_FILES="$MOD_FILE $CT_FILES"
         fi
     else
-        echo "skipped $MOD_FILE since not a cpp .hh or .cc file"
+        echo "skipped $MOD_FILE since not a cpp file"
     fi
 done
 
@@ -30,10 +32,35 @@ ${CLANG_TIDY_RUNNER} ${CLANG_TIDY_ARGS} ${CT_FILES} > $WORKSPACE/clang-tidy-log-
 git checkout -- .clang-tidy
 git checkout -- .clang-format # we do this so these configs do not show up in the diff.
 git checkout -- SConstruct
-git diff > $PATCH_FILE
+# git diff > $PATCH_FILE
 
-if [ -s "$PATCH_FILE" ]; then
-  PURL="${JOB_URL}/${BUILD_NUMBER}/artifact/clang-format-pr${PULL_REQUEST}-${COMMIT_SHA}.patch"
+# if [ -s "$PATCH_FILE" ]; then
+#   PURL="${JOB_URL}/${BUILD_NUMBER}/artifact/clang-format-pr${PULL_REQUEST}-${COMMIT_SHA}.patch"
+
+#   cat > $WORKSPACE/gh-report.md <<- EOM
+# ${COMMIT_SHA}
+# mu2e/codechecks
+# success
+# Code checks have finished.
+# ${JOB_URL}/${BUILD_NUMBER}/console
+# :cloud: clang-format generated a patch at ref ${COMMIT_SHA} on files you changed.
+# #### clang-tidy results
+# The \`clang-tidy\` log file is [here](${JOB_URL}/${BUILD_NUMBER}/artifact/clang-tidy-log-${COMMIT_SHA}.log).
+
+# #### clang-format suggests re-formatting files:
+# Please review the patch [here]($PURL).
+
+# If it is convenient to do so, you can apply the patch like this:
+# \`\`\`
+# curl $PURL | git apply -v --index
+# git commit -am "Code formatting patch on ${COMMIT_SHA}" && git push
+# \`\`\`
+
+# EOM
+
+#     exit 1;
+# fi
+
 
   cat > $WORKSPACE/gh-report.md <<- EOM
 ${COMMIT_SHA}
@@ -41,35 +68,8 @@ mu2e/codechecks
 success
 Code checks have finished.
 ${JOB_URL}/${BUILD_NUMBER}/console
-:cloud: clang-format generated a patch at ref ${COMMIT_SHA} on files you changed.
-#### clang-tidy results
-The \`clang-tidy\` log file is [here](${JOB_URL}/${BUILD_NUMBER}/artifact/clang-tidy-log-${COMMIT_SHA}.log).
-
-#### clang-format suggests re-formatting files:
-Please review the patch [here]($PURL).
-
-If it is convenient to do so, you can apply the patch like this:
-\`\`\`
-curl $PURL | git apply -v --index
-git commit -am "Code formatting patch on ${COMMIT_SHA}" && git push
-\`\`\`
-
-EOM
-
-    exit 1;
-fi
-
-
-  cat > $WORKSPACE/gh-report.md <<- EOM
-${COMMIT_SHA}
-mu2e/codechecks
-success
-Code checks have finished.
-${JOB_URL}/${BUILD_NUMBER}/console
-:sunny: No re-formatting is required at ref ${COMMIT_SHA} on files you changed.
-
-#### clang-tidy results
-The \`clang-tidy\` log file is [here](${JOB_URL}/${BUILD_NUMBER}/artifact/clang-tidy-log-${COMMIT_SHA}.log).
+Clang-tidy was run on $(echo "$CT_FILES" | wc -w) of $($MODIFIED_PR_FILES | wc -w) modified files, at ref ${COMMIT_SHA}.
+Please review the log file [here](${JOB_URL}/${BUILD_NUMBER}/artifact/clang-tidy-log-${COMMIT_SHA}.log).
 
 EOM
 exit 0;
