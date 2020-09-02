1601343a0ff27ec51820435b0b01866a2e915cf7
Ryunosuke O'Neil <roneil@fnal.gov>
2020-09-03 00:06:02 +0100

    hard tab detection

--- bin/github/jenkins_tests/mu2e-offline-code-checks/checks.sh
+++ bin/github/jenkins_tests/mu2e-offline-code-checks/checks.sh
@@ -40,6 +40,41 @@ EOM
     exit 1;
 fi
 
+echo "[`date`] find hard tabs"
+HARD_TABS=0
+for MOD_FILE in $MODIFIED_PR_FILES
+do
+    if [[ "$MOD_FILE" == *.cc ]] || [[ "$MOD_FILE" == *.hh ]]; then
+        detect-tab-indent "$MOD_FILE" >> ${WORKSPACE}/detect-tab-indent-${COMMIT_SHA}.log
+        
+        if [ $? -ne 0 ]; then
+            HARD_TABS=1
+        fi
+    else
+        echo "skipped $MOD_FILE since not a cpp file"
+    fi
+done
+
+if [ $HARD_TABS -ne 0 ]; then
+  PURL="${JOB_URL}/${BUILD_NUMBER}/artifact/detect-tab-indent-${COMMIT_SHA}.log"
+  cat > $WORKSPACE/gh-report.md <<- EOM
+${COMMIT_SHA}
+mu2e/codechecks
+failure
+Hard tabs were found in one or more header or source files.
+${JOB_URL}/${BUILD_NUMBER}/console
+:cloud: Hard tab indentations were found at ${COMMIT_SHA} on files you changed.
+
+Please remove any tab characters from code indentations, and re-indent with spaces where appropriate. 
+
+See the [log file]($PURL) for more details.
+
+EOM
+
+    exit 1;
+fi
+
+
 
   cat > $WORKSPACE/gh-report.md <<- EOM
 ${COMMIT_SHA}
